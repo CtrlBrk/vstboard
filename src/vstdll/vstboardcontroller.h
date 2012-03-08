f159174b6593005b1c6ccc88c47f50937a093d2b
CtrlBrk <ctrlbrk76@gmail.com>
2012-03-08 16:51:38 +0100

    utf8

--- src/vstdll/vstboardcontroller.h
+++ src/vstdll/vstboardcontroller.h
@@ -1,5 +1,5 @@
 /**************************************************************************
-#    Copyright 2010-2012 Raphaël François
+#    Copyright 2010-2012 RaphaÃ«l FranÃ§ois
 #    Contact : ctrlbrk76@gmail.com
 #
 #    This file is part of VstBoard.
