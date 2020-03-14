618cdf56a6695a35478724da0adfde2e7d6f4717
ctrlbrk <ctrlbrk76@gmail.com>
2020-03-14 20:50:46 +0100

    icons

---
+++ src/common/views/keypressedwidget.cpp
@@ -0,0 +1,6 @@
+#include "keypressedwidget.h"
+
+KeypressedWidget::KeypressedWidget()
+{
+
+}
---
+++ src/common/views/keypressedwidget.h
@@ -0,0 +1,13 @@
+#ifndef KEYPRESSEDWIDGET_H
+#define KEYPRESSEDWIDGET_H
+
+#include <QWidget>
+
+class KeypressedWidget : public QLineEdit
+{
+    Q_OBJECT
+public:
+    KeypressedWidget();
+};
+
+#endif // KEYPRESSEDWIDGET_H
