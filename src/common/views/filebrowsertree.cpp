e7ee9027dd7500272db77640ab7e2ba051d29226
ctrlbrk <ctrlbrk76@gmail.com>
2020-04-19 16:32:56 +0200

    file browser click event fixed ?

--- src/common/views/filebrowsertree.cpp
+++ src/common/views/filebrowsertree.cpp
@@ -11,8 +11,14 @@ void FileBrowserTree::mouseReleaseEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::BackButton) {
         emit historyBack();
+        event->setAccepted(true);
+        return;
     }
     if (event->button() == Qt::ForwardButton) {
         emit historyForward();
+        event->setAccepted(true);
+        return;
     }
+
+    QTreeView::mouseReleaseEvent(event);
 }
